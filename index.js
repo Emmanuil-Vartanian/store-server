dca38578336d177969f618dfa5deff92b5a88600
Emmanuil <vartanyanemanyil@gmail.com>
2020-12-19 13:39:05 +0200

    5

--- index.js
+++ index.js
@@ -5,12 +5,11 @@ let http = require("request");
 // const { graphqlHTTP } = require("express-graphql");
 const app = express();
 
-const mailer = require("./smtpGmail");
 // const { schema } = require("./graphQL/schema/schema");
 // const { root } = require("./graphQL/resolvers/resolvers");
 
 app.use(bodyParser.json());
-app.use(express.static("build"));
+// app.use(express.static("build"));
 app.use(cors());
 
 var users = [];
@@ -30,9 +29,9 @@ app.post("/user", (req, res) => {
     "<b>Доставка</b>: " + reqBody.delivery,
     "<b>Город</b>: " + reqBody.city,
     "<b>Отделение новой почты</b>: " + reqBody.newMail,
-    "<b>Название игрушек</b>: " + reqBody.dataToys.map((obj) => obj.title),
-    "<b>Цена игрушек</b>: " + reqBody.dataToys.map((obj) => obj.price),
-    "<b>Цвет игрушек</b>: " +
+    "<b>Название игрушки</b>: " + reqBody.dataToys.map((obj) => obj.title),
+    "<b>Цена игрушки</b>: " + reqBody.dataToys.map((obj) => obj.price),
+    "<b>Цвет игрушки</b>: " +
       reqBody.dataToys.map((obj) =>
         obj.colorsToy === "" ? "натуральный цвет" : obj.colorsToy
       ),
@@ -61,30 +60,6 @@ app.post("/user", (req, res) => {
   res.status(201) || res.status(200);
 });
 
-// app.post("/user", (req, res) => {
-//   users.push(req.body);
-//   console.log(req.body);
-//   const message = {
-//     to: "super.detskieigruski@gmail.com",
-//     subject: "Заказ",
-//     text: `Данные о заказе:
-//       Имя: ${req.body.firstName}
-//       Фамилия: ${req.body.lastName}
-//       Телефон: ${req.body.telephone}
-//       Доставка: ${req.body.delivery}
-//       Город: ${req.body.city}
-//       Отделение новой почты: ${req.body.newMail}
-
-//       Название игрушек: ${req.body.dataToys.map((obj) => obj.title)}
-//       Цена игрушек: ${req.body.dataToys.map((obj) => obj.price)}
-//       Цвет игрушек: ${req.body.dataToys.map((obj) =>
-//         obj.colorsToy === "" ? "натуральный цвет" : obj.colorsToy
-//       )}`
-//   };
-//   mailer(message);
-//   res.status(201).send(req.body);
-// });
-
 // app.use(
 //   "/graphql",
 //   graphqlHTTP({
@@ -93,26 +68,5 @@ app.post("/user", (req, res) => {
 //     graphiql: true,
 //   })
 // );
-
-// app.get("/user", (req, res) => {
-//   res.send(JSON.stringify(users));
-// });
-
-// app.post("/user", (req, res) => {
-//   users.push(req.body);
-//   console.log(req.body);
-//   const message = {
-//     to: "super.detskieigruski@gmail.com",
-//     subject: "Заказ",
-//     text: `Данные о заказе:
-//       Имя: ${req.body.userName}
-//       Фамилия: ${req.body.userLastName}
-//       Город: ${req.body.userCity}
-//       Отделение новой почты: ${req.body.userNewMail}
-//       Телефон: ${req.body.userPhone}`
-//   };
-//   mailer(message);
-//   res.status(201).send(req.body);
-// });
 
 app.listen(5556, () => console.log("The server started on port 5556"));
